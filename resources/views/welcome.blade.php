e56aec66a8a22d672c6232b5895ba2a5f1bf1220
Pugazhenthi <pugazhwoo@gmail.com>
2022-12-14 23:34:04 +0530

    WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,18 +12,35 @@
         </div>
         <script>
             document.querySelectorAll("[wire\\:snapshot]").forEach((el) => {
-                let snapshot = JSON.parse(el.getAttribute("wire:snapshot"));
+                el.__livewire = JSON.parse(el.getAttribute("wire:snapshot"));
+
+                initWireCliet(el);
+            });
+
+            function initWireCliet(el) {
                 el.addEventListener("click", (e) => {
                     if (!e.target.hasAttribute("wire:click")) return;
                     let method = e.target.getAttribute("wire:click");
-                    fetch("/livewire", {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({ snapshot, callMethod: method }),
-                    });
+
+                    sendRequest(el, { callMethod: method });
                 });
-                console.log(snapshot);
-            });
+            }
+
+            function sendRequest(el, addToPayload) {
+                snapshot = el.__livewire;
+
+                fetch("/livewire", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ snapshot, ...addToPayload }),
+                })
+                    .then((i) => i.json())
+                    .then((response) => {
+                        let { snapshot, html } = response;
+                        el.__livewire = snapshot;
+                        el.innerHTML = html;
+                    });
+            }
         </script>
     </body>
 </html>
